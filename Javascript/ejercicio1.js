2272853a6d57aceb520a416a9bfdecdb64efeb8b
Jesus Angarita <jes.angar11@gmail.com>
2024-08-05 19:47:42 -0500

    avance 2 lunes 05

--- Javascript/ejercicio1.js
+++ Javascript/ejercicio1.js
@@ -40,25 +40,26 @@ if (Pedro >= 0.6 && Pedro <= 0.9) {
 
 Cofla = prompt("cuanto dinero tienes Cofla?");
 
-if (Cofla >= 0.6 && Cofla <= 0.9) {
+if (Cofla >= 0.6 && Cofla < 1) {
   alert("puedes comprar el palito de agua");
-  alert("te sobraron "(Cofla - 0.6));
-} else if (Cofla >= 1 && Cofla <= 1.5) {
+  alert("te sobraron " + (Cofla - 0.6));
+} else if (Cofla >= 1 && Cofla < 1.6) {
   alert("puedes comprar el palito de helado de crema");
-  alert("te sobraron "(Cofla - 1));
-} else if (Cofla >= 1.6) {
-  alert(
-    "puedes comprar el Bombon helado marca heladix o el Bombon helado marca helovich"
-  );
-  alert("te sobraron "(Cofla - 0.6));
-} else if (Cofla >= 1.8 && Cofla <= 2.5) {
+  alert("te sobraron " + (Cofla - 1));
+} else if (Cofla >= 1.6 && Cofla < 1.7) {
+  alert("puedes comprar el Bombon helado marca heladix ");
+  alert("te sobraron " + (Cofla - 1.6));
+} else if (Cofla >= 1.7 && Cofla < 1.8) {
+  alert("puedes comprar el Bombon helado marca helovich");
+  alert("te sobraron " + (Cofla - 1.7));
+} else if (Cofla >= 1.8 && Cofla <= 2.9) {
   alert("puedes comprar el el Bombon helado marca helardo");
-  alert("te sobraron "(Cofla - 0.6));
-} else if (Cofla > 2.6) {
+  alert("te sobraron " + (Cofla - 1.8));
+} else if (Cofla > 2.9) {
   alert(
     "puedes comprar el potecito de helado con confites o el pote de 1/4KG de helado"
   );
-  alert("te sobraron "(Cofla - 0.6));
+  alert("te sobraron " + (Cofla - 2.9));
 } else {
   alert("no te alcanza para un helado");
 }
